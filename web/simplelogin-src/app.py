3c9c5b755d8a3ff02e26e35666946649f69e76f6
abiramen <7523422+abiramen@users.noreply.github.com>
2021-01-06 03:11:44 +1100

    Add web challenge src

---
+++ web/simplelogin-src/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, make_response
+app = Flask(__name__)
+
+PASSWORD = 'ireALLyLikEsPaceShiPs207$'
+EXPECTED = 'aXJlQUxMeUxpa0VzUGFjZVNoaVBzMjA3JA=='
+MESSAGE = f'Password did not match expected encoded result: <kbd>{EXPECTED}</kbd>'
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        if request.form['username'] == 'spaceman' and request.form['password'] == PASSWORD:
+            resp = make_response(render_template("index.html", correct='True'))
+        else:
+            resp = make_response(render_template("index.html", correct='False', message=MESSAGE))
+    else:
+        resp = make_response(render_template("index.html", correct='False', message=''))
+
+    resp.set_cookie("cookies-are-yum", value=r"FLAG{1_i_cHeCkeD_f0r_c0oki3S}")
+    return resp
+
+if __name__ == '__main__':
+    app.run(port=1337)
