55bc018264c7d8c7ba36a1bacf81ebfbcc316724
abiramen <7523422+abiramen@users.noreply.github.com>
2021-01-11 18:39:41 +1100

    Improve error message

--- web/simplelogin-src/app/views.py
+++ web/simplelogin-src/app/views.py
@@ -3,7 +3,7 @@ from app import app
 
 PASSWORD = 'ireALLyLikEsPaceShiPs207$'
 EXPECTED = 'aXJlQUxMeUxpa0VzUGFjZVNoaVBzMjA3JA=='
-MESSAGE = f'Password did not match expected encoded result: <kbd>{EXPECTED}</kbd>'
+MESSAGE = f'Password did not match expected encoded result: <kbd>{EXPECTED}</kbd>, or username did not match `astronaut`.'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
